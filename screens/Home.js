a82c0c46863bb55e9743d4294fea1a3e1df70e23
tramtran0497 <tramtran0497@gmail.com>
2022-05-30 17:29:04 +0300

    Installing, creating screens Home and Details

---
+++ screens/Home.js
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import React from 'react'
+import { Text, SafeArea, FlatList, View } from 'react-native'
+
+const Home = () => {
+  return (
+    <Text>Home</Text>
+  )
+}
+
+export default Home
