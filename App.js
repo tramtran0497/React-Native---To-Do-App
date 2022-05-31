ba62b695ba450c89fd2b75ac561dfcd317285762
tramtran0497 <tramtran0497@gmail.com>
2022-05-31 23:10:17 +0300

    Getting back TodoApp and design main page

--- App.js
+++ App.js
@@ -1,42 +1,45 @@
-import { useState } from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
-import {Text, View } from 'react-native'
-import { useFonts } from 'expo-font'
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+import Task from "./components/Task"
 
-import Home from './screens/Home'
-import Details from './screens/Details'
-
-const Stack = createStackNavigator()
-
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    background: "transparent"
-  }
-}
-
-const App = () => {
-  const [loaded] = useFonts({
-    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
-    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
-    InterLight: require("./assets/fonts/Inter-Light.ttf"),
-    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
-    InterRegular: require("./assets/fonts/Inter-Regular.ttf")
-  })
-
-  if(!loaded) return null
+export default function App(){
   return (
-    <NavigationContainer theme={theme}>
-      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home}/>
-        <Stack.Screen name="Details" component={Details}/>
-
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      {/* Today's Tasks */}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+          <View style={styles.listTasks}>
+                    {/* Task is here! */}
+            <Task/>
+          </View>
+      </View>
+    </View>
   )
-}
+}  
 
-export default App
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "pink",
+  },
+
+  taskWrapper: {
+    
+  },
+
+  sectionTitle: {
+    paddingTop: 80,
+    paddingLeft: 20,
+    fontSize: 24,
+    fontWeight: "500",
+    paddingBottom: 10,
+    backgroundColor: "#f64f59",
+
+  },
+
+  listTasks: {
+    paddingTop: 30,
+    paddingLeft: 20,
+  }
+})
  
---
+++ components/Task.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+
+const Task = () => {
+  return (
+    <View>
+      <Text>A Task!</Text>
+    </View>
+  )
+}  
+
+const styles = StyleSheet.create({
+  
+})
+
+export default Task
+ 
