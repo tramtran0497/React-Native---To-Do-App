66b070123dbb076ad3e20f678ed6f62be067b07a
tramtran0497 <tramtran0497@gmail.com>
2022-06-03 23:57:01 +0300

    adding place to write task with styles

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity} from "react-native"
 import Task from "./components/Task"
 
 export default function App(){
@@ -15,6 +15,19 @@ export default function App(){
 
           </View>
       </View>
+
+       {/* Write Tasks */}
+       <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTasksWrapper} >
+        <Text>LALALALAL</Text>
+        <TextInput style={styles.input} placeholder="Leave your task here!"/>
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+       </KeyboardAvoidingView>
     </View>
   )
 }  
